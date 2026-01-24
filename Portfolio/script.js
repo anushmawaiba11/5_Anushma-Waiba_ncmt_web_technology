808e8daef13268f929cea5565d656c7de42a92a9
Anushma Waiba <anushmawaiba11@gmail.com>
2026-01-24 21:20:09 +0545

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function (e) {
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            e.preventDefault();
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
